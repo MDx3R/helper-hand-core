599ad086145758beec764cd81e2908c10c7b0dea
Dimitriy <nationmdxr@gmail.com>
2025-03-11 21:54:22 +0300

    add mapper to dtos for user roles

---
+++ application/dtos/mappers.py
@@ -0,0 +1,12 @@
+from domain.models import User
+from domain.models.enums import RoleEnum
+
+from application.dtos.output import UserOutputDTO, ContracteeOutputDTO, ContractorOutputDTO, AdminOutputDTO
+
+def map_user_to_dto(user: User) -> UserOutputDTO | None:
+    mapping = {
+        RoleEnum.contractee: ContracteeOutputDTO.from_contractee,
+        RoleEnum.contractor: ContractorOutputDTO.from_contractor,
+        RoleEnum.admin: AdminOutputDTO.from_admin,
+    }
+    return mapping.get(user.role, lambda x: None)(user)
