42508f89544420687212f8355294883aa7765fc9
Dimitriy <nationmdxr@gmail.com>
2025-03-14 18:45:14 +0300

    add comments for model fields

--- domain/models/available_replies_for_detail.py
+++ domain/models/available_replies_for_detail.py
@@ -2,7 +2,10 @@ from .base import ApplicationModel
 
 class AvailableRepliesForDetail(ApplicationModel):
     detail_id: int
+    """Идентификатор позиции."""
+
     quantity: int
+    """Количество свободных мест на позицию."""
 
     def is_full(self) -> bool:
         return self.quantity <= 0
