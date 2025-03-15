6c6f9562a6d0d7e7471fc53e253f177adeb9516d
Dimitriy <nationmdxr@gmail.com>
2025-03-15 13:03:04 +0300

    refactor domain services and add more methods

--- domain/services/domain/services.py
+++ domain/services/domain/services.py
@@ -24,32 +24,64 @@ class OrderDomainService:
         return order.supervisor_id is not None
 
     @staticmethod
-    def is_available(order: Order) -> bool:
-        return order.status == OrderStatusEnum.open
-
-    @staticmethod
-    def can_be_assigned(order: Order) -> bool:
-        return order.supervisor_id is None and order.status == OrderStatusEnum.created
-
-    @staticmethod
-    def can_be_approved(order: Order) -> bool:
+    def is_created(order: Order) -> bool:
         return order.status == OrderStatusEnum.created
 
     @staticmethod
-    def can_be_cancelled(order: Order) -> bool:
-        return order.status != OrderStatusEnum.fulfilled and order.status != OrderStatusEnum.cancelled
-    
-    @staticmethod
-    def can_be_closed(order: Order) -> bool:
+    def is_open(order: Order) -> bool:
         return order.status == OrderStatusEnum.open
     
     @staticmethod
-    def can_be_opened(order: Order) -> bool:
+    def is_closed(order: Order) -> bool:
         return order.status == OrderStatusEnum.closed
-    
+
     @staticmethod
+    def is_active(order: Order) -> bool:
+        return order.status == OrderStatusEnum.active
+
+    @staticmethod
+    def is_cancelled(order: Order) -> bool:
+        return order.status == OrderStatusEnum.cancelled
+
+    @staticmethod
+    def is_fulfilled(order: Order) -> bool:
+        return order.status == OrderStatusEnum.fulfilled
+
+    @classmethod
+    def is_available(order: Order) -> bool:
+        return OrderDomainService.is_open(order)
+
+    @classmethod
+    def can_be_assigned(order: Order) -> bool:
+        return not OrderDomainService.has_supervisor(order) and OrderDomainService.is_created(order)
+
+    @classmethod
+    def can_be_approved(order: Order) -> bool:
+        return OrderDomainService.is_created(order)
+
+    @classmethod
+    def can_be_cancelled(order: Order) -> bool:
+        return not OrderDomainService.is_fulfilled(order) and not OrderDomainService.is_cancelled(order)
+    
+    @classmethod
+    def can_be_closed(order: Order) -> bool:
+        return OrderDomainService.is_open(order)
+    
+    @classmethod
+    def can_be_opened(order: Order) -> bool:
+        return OrderDomainService.is_closed(order)
+    
+    @classmethod
+    def can_be_set_active(order: Order) -> bool:
+        return OrderDomainService.is_open(order) or OrderDomainService.is_closed(order)
+
+    @classmethod
     def can_be_fulfilled(order: Order) -> bool:
-        return order.status == OrderStatusEnum.closed
+        return OrderDomainService.is_active(order)
+    
+    @classmethod
+    def can_have_replies(order: Order) -> bool:
+        return OrderDomainService.is_open(order)
     
 class OrderDetailDomainService:
     @staticmethod
@@ -65,6 +97,6 @@ class AvailabilityDomainService:
     def is_full(availability: AvailableRepliesForDetail) -> bool:
         return availability.quantity <= 0
     
-    @staticmethod
-    def are_all_full(availabilities: List[AvailableRepliesForDetail]) -> bool:
-        return all(AvailabilityDomainService.is_full(av) for av in availabilities)
+    @classmethod
+    def are_all_full(cls, availabilities: List[AvailableRepliesForDetail]) -> bool:
+        return all(cls.is_full(av) for av in availabilities)
