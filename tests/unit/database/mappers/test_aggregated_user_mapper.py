1a88502c177cee2b1508f3fa18dc018587ffe2c8
Dimitriy <nationmdxr@gmail.com>
2025-03-17 05:40:49 +0300

    adapt for new to_base signature

--- tests/unit/database/mappers/test_aggregated_user_mapper.py
+++ tests/unit/database/mappers/test_aggregated_user_mapper.py
@@ -32,16 +32,16 @@ class TestToModelAggregatedUserMapper:
 class TestToBaseAggregatedUserMapper:
     @pytest.mark.parametrize("mapper, user_base, expected_base, model_instance", test_cases)
     def test_to_base_result_is_correct_instance(self, mapper: AggregatedUserMapper, user_base: UserBase, expected_base, model_instance) -> None:
-        base = mapper.to_base(model_instance)
-        assert isinstance(base, type(expected_base))
+        user_base, role_base = mapper.to_base(model_instance)
+        assert isinstance(role_base, type(expected_base))
 
     @pytest.mark.parametrize("mapper, user_base, expected_base, model_instance", test_cases)
     def test_to_base_result_is_correct(self, mapper: AggregatedUserMapper, user_base: UserBase, expected_base, model_instance) -> None:
-        base = mapper.to_base(model_instance)
-        assert base.get_fields() == expected_base.get_fields()
+        user_base, role_base = mapper.to_base(model_instance)
+        assert role_base.get_fields() == expected_base.get_fields()
 
     @pytest.mark.parametrize("mapper, user_base, expected_base, model_instance", test_cases)
     def test_to_base_preserves_created_at_and_updated_at(self, mapper: AggregatedUserMapper, user_base: UserBase, expected_base, model_instance) -> None:
-        base = mapper.to_base(model_instance)
-        assert base.created_at == expected_base.created_at
-        assert base.updated_at == expected_base.updated_at
+        user_base, role_base = mapper.to_base(model_instance)
+        assert role_base.created_at == expected_base.created_at
+        assert role_base.updated_at == expected_base.updated_at
