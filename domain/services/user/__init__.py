2ef497e3e95ad6e18340b4b5f80aa9d582da17f1
Dimitriy <nationmdxr@gmail.com>
2025-04-01 19:10:28 +0300

    split AdminUserService into logical parts

--- domain/services/user/__init__.py
+++ domain/services/user/__init__.py
@@ -1,3 +1,8 @@
-from .admin_user_service import AdminUserService
+from .admin_user_service import (
+    AdminUserApprovalService,
+    AdminUserNotificationService,
+    AdminUserQueryService,
+    AdminUserManagementService
+)
 from .contractee_user_service import ContracteeUserService
 from .contractor_user_service import ContractorUserService
--- domain/services/user/admin_user_service.py
+++ domain/services/user/admin_user_service.py
@@ -1,37 +1,36 @@
 from abc import ABC, abstractmethod
 
-from domain.entities import User, Admin
-
 from domain.dto.common import UserDTO
+from domain.dto.context import UserContextDTO
 
 class AdminUserQueryService(ABC):
     @abstractmethod
-    async def get_user(self, user_id: int, admin: Admin) -> UserDTO | None:
+    async def get_user(self, user_id: int) -> UserDTO | None:
         pass
 
     @abstractmethod
-    async def get_first_pending_user(self, admin: Admin) -> UserDTO | None:
+    async def get_first_pending_user(self) -> UserDTO | None:
         pass
 
 class AdminUserManagementService(ABC):
     @abstractmethod
-    async def drop_user(self, user_id: int, admin: Admin) -> UserDTO:
+    async def drop_user(self, user_id: int, context: UserContextDTO) -> UserDTO:
         pass
 
     @abstractmethod
-    async def ban_user(self, user_id: int, admin: Admin) -> UserDTO:
+    async def ban_user(self, user_id: int, context: UserContextDTO) -> UserDTO:
         pass
 
 class AdminUserApprovalService(ABC):
     @abstractmethod
-    async def approve_registration(self, user_id: int, admin: Admin) -> UserDTO:
+    async def approve_registration(self, user_id: int, context: UserContextDTO) -> UserDTO:
         pass
 
     @abstractmethod
-    async def disapprove_registration(self, user_id: int, admin: Admin) -> UserDTO:
+    async def disapprove_registration(self, user_id: int, context: UserContextDTO) -> UserDTO:
         pass
 
 class AdminUserNotificationService(ABC):
     @abstractmethod
-    async def notify_user(self, user_id: int, admin: Admin):
+    async def notify_user(self, user_id: int, context: UserContextDTO):
         pass
