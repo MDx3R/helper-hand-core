52e721e8d4a00889943940b21c6b901e122f2f52
Dimitriy <nationmdxr@gmail.com>
2025-04-02 15:59:15 +0300

    replace int params to DTOs

--- application/usecases/user/user_query_use_case.py
+++ application/usecases/user/user_query_use_case.py
@@ -7,32 +7,34 @@ from application.transactions import transactional
 
 from domain.dto.mappers import map_user_to_dto
 
+from domain.dto.internal import GetUserDTO
+
 class GetUserUseCase(ABC):
     @abstractmethod
-    async def get_user(self, user_id: int) -> UserDTO | None:
+    async def get_user(self, query: GetUserDTO) -> UserDTO | None:
         pass
 
 class GetUserWithRoleUseCase(ABC):
     @abstractmethod
     async def get_user_with_role(
         self, 
-        user_id: int
+        query: GetUserDTO
     ) -> ContracteeDTO | ContractorDTO | AdminDTO | None:
         pass
 
 class GetAdminUseCase(ABC):
     @abstractmethod
-    async def get_admin(self, admin_id: int) -> AdminDTO | None:
+    async def get_admin(self, query: GetUserDTO) -> AdminDTO | None:
         pass
 
 class GetContracteeUseCase:
     @abstractmethod
-    async def get_contractee(self, contractee_id: int) -> ContracteeDTO | None:
+    async def get_contractee(self, query: GetUserDTO) -> ContracteeDTO | None:
         pass
 
 class GetContractorUseCase:
     @abstractmethod
-    async def get_contractor(self, contractor_id: int) -> ContractorDTO | None:
+    async def get_contractor(self, query: GetUserDTO) -> ContractorDTO | None:
         pass
 
 class GetPendingUserUseCase:
@@ -52,8 +54,8 @@ class UserQueryUseCaseFacade(
     ):
         self.user_repository = user_repository
 
-    async def get_user(self, user_id: int) -> UserDTO | None:
-        user = await self.user_repository.get_user(user_id)
+    async def get_user(self, query: GetUserDTO) -> UserDTO | None:
+        user = await self.user_repository.get_user(query.user_id)
         if not user:
             return None
         
@@ -61,30 +63,30 @@ class UserQueryUseCaseFacade(
     
     async def get_user_with_role(
         self, 
-        user_id: int
+        query: GetUserDTO
     ) -> ContracteeDTO | ContractorDTO | AdminDTO | None:
-        user = await self.user_repository.get_user_with_role(user_id)
+        user = await self.user_repository.get_user_with_role(query.user_id)
         if not user:
             return None
         
         return map_user_to_dto(user)
 
-    async def get_admin(self, admin_id: int) -> AdminDTO | None:
-        admin = await self.user_repository.get_admin(admin_id)
+    async def get_admin(self, query: GetUserDTO) -> AdminDTO | None:
+        admin = await self.user_repository.get_admin(query.user_id)
         if not admin:
             return None
         
         return AdminDTO.from_admin(admin)
     
-    async def get_contractee(self, contractee_id: int) -> ContracteeDTO | None:
-        contractee = await self.user_repository.get_contractee(contractee_id)
+    async def get_contractee(self, query: GetUserDTO) -> ContracteeDTO | None:
+        contractee = await self.user_repository.get_contractee(query.user_id)
         if not contractee:
             return None
         
         return ContracteeDTO.from_contractee(contractee)
     
-    async def get_contractor(self, contractor_id: int) -> ContractorDTO | None:
-        contractor = await self.user_repository.get_contractor(contractor_id)
+    async def get_contractor(self, query: GetUserDTO) -> ContractorDTO | None:
+        contractor = await self.user_repository.get_contractor(query.user_id)
         if not contractor:
             return None
         
