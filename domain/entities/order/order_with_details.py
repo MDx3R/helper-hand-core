4a1c66a7e33d36757f72d228947ab6c5fb30bb66
MDx3R <nationmdxr@gmail.com>
2025-04-21 01:26:13 +0300

    add more composite order entities

--- domain/entities/order/order_with_details.py
+++ domain/entities/order/order_with_details.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import List, Optional
 
 from domain.entities.base import ApplicationModel
+from domain.entities.user.admin import Admin
+from domain.entities.user.contractor import Contractor
 
 from .detail import OrderDetail
 from .order import Order
@@ -13,3 +15,34 @@ class OrderWithDetails(ApplicationModel):
 
     order: Order
     details: List[OrderDetail]
+
+
+class OrderWithDetailsAndContractor(ApplicationModel):
+    """
+    Композитная модель заказа, его сведений и владельца этого заказа.
+    """
+
+    order: Order
+    details: List[OrderDetail]
+    contractor: Contractor
+
+
+class OrderWithDetailsAndSupervisor(ApplicationModel):
+    """
+    Композитная модель заказа, его сведений и куратора этого заказа.
+    """
+
+    order: Order
+    details: List[OrderDetail]
+    admin: Optional[Admin]
+
+
+class CompleteOrder(ApplicationModel):
+    """
+    Композитная модель заказа и связанных с ним сущностей.
+    """
+
+    order: Order
+    details: List[OrderDetail]
+    contractor: Contractor
+    admin: Optional[Admin]
