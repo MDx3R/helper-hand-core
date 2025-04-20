21c66b8cb607e86f2f73441f9a6fb258e9d7cb42
MDx3R <nationmdxr@gmail.com>
2025-04-20 22:40:35 +0300

    separate user repositories and change method signatures

--- domain/repositories/user_repository.py
+++ domain/repositories/user_repository.py
@@ -1,230 +1,116 @@
-from typing import List
 from abc import ABC, abstractmethod
+from typing import TypeVar, List
+from domain.dto.user.internal.base import (
+    UserIdDTO,
+    UserWithCredentialsDTO,
+)
+from domain.dto.user.internal.user_command_dto import SetUserStatusDTO
+from domain.dto.user.internal.user_filter_dto import (
+    AdminFilterDTO,
+    ContracteeFilterDTO,
+    ContractorFilterDTO,
+    UserFilterDTO,
+)
+from domain.entities.user.admin import Admin
+from domain.entities.user.contractee import Contractee
+from domain.entities.user.contractor import Contractor
+from domain.entities.user.telegram_user import TelegramUser
+from domain.entities.user.user import User
 
-from domain.entities import User, Admin, Contractee, Contractor, TelegramUser
-from domain.entities.enums import UserStatusEnum, RoleEnum, GenderEnum
+Role = TypeVar("Role", Admin, Contractee, Contractor)
 
-class UserRepository(ABC):
+
+class UserQueryRepository(ABC):
     @abstractmethod
-    async def get_user(self, user_id: int) -> User | None:
-        """
-        Получает пользователя по его ID.
-        
-        Args:
-            user_id (int): ID пользователя.
-            
-        Returns:
-            User: Модель с данными пользователя или `None`, если пользователь не найден.
-        """
-        pass
-    
-    @abstractmethod
-    async def get_user_by_telegram_id(self, telegram_id: int) -> User | None:
-        """
-        Получает пользователя по его ID в Telegram. 
-        
-        Args:
-            telegram_id (int): ID пользователя в Telegram.
-            
-        Returns:
-            User: Модель с данными пользователя или `None`, если пользователь не найден.
-        """
-        pass
-    
-    @abstractmethod
-    async def get_user_with_role(self, user_id: int) -> User | None:
-        """
-        Получает роль пользователя по его ID.
-        
-        Args:
-            user_id (int): ID пользователя.
-            
-        Returns:
-            User: Модель роли, расширяющая User.
-        """
+    async def get_user(self, query: UserIdDTO) -> User | None:
         pass
 
     @abstractmethod
-    async def get_admin(self, admin_id: int) -> Admin | None:
-        """
-        Получает администратора по его ID.
-        
-        Args:
-            admin_id (int): ID администратора.
-        
-        Returns:
-            Admin: Модель с данными администратора или `None`, если администратор не найден.
-        """
-        pass
-    
-    @abstractmethod
-    async def get_contractee(self, contractee_id: int) -> Contractee | None:
-        """
-        Получает исполнителя по его ID.
-        
-        Args:
-            contractee_id (int): ID исполнителя.
-        
-        Returns:
-            Contractee: Модель с данными исполнителя или `None`, если исполнитель не найден.
-        """
-        pass
-    
-    @abstractmethod
-    async def get_contractor(self, contractor_id: int) -> Contractor | None:
-        """
-        Получает заказчика по его ID.
-        
-        Args:
-            contractor_id (int): ID заказчика.
-        
-        Returns:
-            Contractor: Модель с данными заказчика или `None`, если подрядчик не найден.
-        """
-        pass
-    
-    @abstractmethod
-    async def get_admins(self) -> List[Admin]:
-        """
-        Получает список администраторов.
-        
-        Returns:
-            List[Admin]: Список объектов администраторов.
-        """
+    async def filter_users(self, query: UserFilterDTO) -> List[User]:
         pass
 
     @abstractmethod
-    async def get_first_pending_user_with_role(self) -> User | None:
-        """
-        Получает первого пользователя, ждущего подтверждение регистрации.
-            
-        Returns:
-            User: Модель роли, расширяющей User.
-        """
+    async def get_user_with_credentials(
+        self, query: UserWithCredentialsDTO
+    ) -> User | None:
         pass
 
     @abstractmethod
-    async def save_telegram_user(self, user: TelegramUser) -> TelegramUser:
-        """
-        Сохраняет данные профиля Telegram пользователя.
-        
-        Args:
-            user (TelegramUser)
-        
-        Returns:
-            TelegramUser.
-            
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            DuplicateEntryException: Возникает, если уникальные значения повторяются: Если произошел конфликт уникальности поля.
-        """
+    async def exists_by_query(self, query: UserFilterDTO) -> bool:
+        pass
+
+
+class AdminQueryRepository(ABC):
+    @abstractmethod
+    async def get_admin(self, query: UserIdDTO) -> Admin | None:
         pass
 
     @abstractmethod
-    async def save(self, role: Contractee | Contractor | Admin) -> Contractee | Contractor | Admin:
-        """
-        Сохраняет данные исполнителя, заказчика или администратора.
-        
-        Args:
-            role (Contractee | Contractor | Admin): Модель с данными пользователя.
-        
-        Returns:
-            Contractee | Contractor | Admin: Сохраненная модель пользователя.
-            
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            DuplicateEntryException: Возникает, если уникальные значения повторяются: Если произошел конфликт уникальности поля.
-        """
+    async def filter_admins(self, query: AdminFilterDTO) -> List[Admin]:
+        pass
+
+
+class ContractorQueryRepository(ABC):
+    @abstractmethod
+    async def get_contractor(self, query: UserIdDTO) -> Contractor | None:
         pass
 
     @abstractmethod
-    async def save_contractee(self, contractee: Contractee) -> Contractee:
-        """
-        Сохраняет данные исполнителя.
-        
-        Args:
-            contractee (Contractee): Модель с данными исполнителя.
-        
-        Returns:
-            Contractee: Сохраненная модель исполнителя.
-            
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            DuplicateEntryException: Возникает, если уникальные значения повторяются: Если произошел конфликт уникальности поля.
-        """
+    async def filter_contractors(self, query: ContractorFilterDTO) -> List[Contractor]:
         pass
-    
+
+
+class ContracteeQueryRepository(ABC):
     @abstractmethod
-    async def save_contractor(self, contractor: Contractor) -> Contractor:
-        """
-        Сохраняет данные заказчика.
-        
-        Args:
-            contractor (Contractor): Модель с данными заказчика.
-        
-        Returns:
-            Contractor: Сохраненная модель заказчика.
-            
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            DuplicateEntryException: Возникает, если уникальные значения повторяются: Если произошел конфликт уникальности поля.
-        """
+    async def get_contractee(self, query: UserIdDTO) -> Contractee | None:
         pass
 
     @abstractmethod
-    async def save_admin(self, admin: Admin) -> Admin:
-        """
-        Сохраняет данные администратора.
-        
-        Args:
-            admin (Admin): Модель с данными администратора.
-        
-        Returns:
-            Admin: Сохраненная модель администратора.
-            
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            DuplicateEntryException: Возникает, если уникальные значения повторяются: Если произошел конфликт уникальности поля.
-        """
+    async def filter_contractees(self, query: ContracteeFilterDTO) -> List[Contractee]:
         pass
-    
-    @abstractmethod
-    async def change_user_status(self, user_id: int, status: UserStatusEnum) -> User:
-        """
-        Изменяет статус пользователя.
-        
-        Args:
-            user_id (int): ID пользователя.
-            status (UserStatusEnum): новый статус пользователя.
-            
-        Returns:
-            User: Изменённый пользователь.
 
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-        """
+
+class UserRoleQueryRepository(ABC):
+    @abstractmethod
+    async def get_user(self, query: UserIdDTO) -> Role | None:
         pass
 
     @abstractmethod
-    async def user_exists_by_phone_number(self, phone_number: str) -> bool:
-        """
-        Проверяет, существует ли пользователь с данным номером телефона.
-        
-        Args:
-            phone_number (str): Номер телефона пользователя.
-        
-        Returns:
-            bool: `True`, если пользователь существует, иначе `False`.
-        """
+    async def get_first_pending_user(
+        self,
+    ) -> Contractee | Contractor | None:
+        pass
+
+
+class UserCommandRepository(ABC):
+    @abstractmethod
+    async def set_user_status(self, query: SetUserStatusDTO) -> None:
         pass
 
     @abstractmethod
-    async def filter_contractees_by(self, status: UserStatusEnum = None, gender: GenderEnum = None) -> List[Contractee]:
-        """
-        Возвращает отфильтрованный список исполнителей.
-        
-        Returns:
-            List[Contractee]
-        """
-        # todo: реализовать батчевый подход к сбору данных
+    async def create_telegram_user(self, user: TelegramUser) -> TelegramUser:
+        pass
+
+
+class AdminCommandRepository(ABC):
+    @abstractmethod
+    async def create_admin(self, admin: Admin) -> Admin:
+        pass
+
+
+class ContractorCommandRepository(ABC):
+    @abstractmethod
+    async def create_contractor(self, contractor: Contractor) -> Contractor:
+        pass
+
+
+class ContracteeCommandRepository(ABC):
+    @abstractmethod
+    async def create_contractee(self, contractee: Contractee) -> Contractee:
+        pass
+
+
+class UserRoleCommandRepository(ABC):
+    @abstractmethod
+    async def update_user(self, role: Role) -> Role:
         pass
