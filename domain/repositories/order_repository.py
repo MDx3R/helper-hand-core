5cc0d5a5410f83a10ecc1e3bce803ed94f6ad40c
MDx3R <nationmdxr@gmail.com>
2025-04-21 12:27:03 +0300

    update signatures with order dtos

--- domain/repositories/order_repository.py
+++ domain/repositories/order_repository.py
@@ -1,7 +1,15 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from domain.entities.order.enums import OrderStatusEnum
+from domain.dto.order.internal.base import DetailIdDTO, OrderIdDTO, OrderSignatureDTO
+from domain.dto.order.internal.order_filter_dto import (
+    ContracteeOrderFilterDTO,
+    OrderFilterDTO,
+)
+from domain.dto.order.internal.user_command_dto import (
+    SetOrderAdminDTO,
+    SetOrderStatusDTO,
+)
 from domain.entities.order.order import Order
 from domain.entities.order.order_with_details import (
     CompleteOrder,
@@ -15,84 +23,82 @@ from domain.entities.user.contractor import Contractor
 
 class OrderQueryRepository(ABC):
     @abstractmethod
-    async def get_order(self, order_id: int) -> Order | None:
+    async def get_order(self, query: OrderIdDTO) -> Order | None:
         pass
 
     @abstractmethod
-    async def get_order_by_detail_id(self, detail_id: int) -> Order | None:
+    async def get_order_by_detail_id(self, query: DetailIdDTO) -> Order | None:
         pass
 
     @abstractmethod
     async def get_order_by_id_and_contractor_id(
-        self, order_id: int, contractor_id: int
+        self, query: OrderSignatureDTO
     ) -> Order | None:
         pass
 
     @abstractmethod
-    async def filter_orders(self, page: int = 1, size: int = None) -> List[Order]:
+    async def filter_orders(self, query: OrderFilterDTO) -> List[Order]:
         pass
 
 
 class CompositeOrderQueryRepository(ABC):
     @abstractmethod
-    async def get_order_with_details(self, order_id: int) -> OrderWithDetails | None:
+    async def get_order_with_details(
+        self, query: OrderIdDTO
+    ) -> OrderWithDetails | None:
         pass
 
     @abstractmethod
-    async def get_complete_order(self, order_id: int) -> CompleteOrder | None:
+    async def get_complete_order(self, query: OrderIdDTO) -> CompleteOrder | None:
         pass
 
     @abstractmethod
     async def filter_orders_with_details(
-        self, page: int = 1, size: int = None
+        self, query: OrderFilterDTO
     ) -> List[OrderWithDetails]:
         pass
 
     @abstractmethod
     async def filter_complete_orders(
-        self, page: int = 1, size: int = None
+        self, query: OrderFilterDTO
     ) -> List[CompleteOrder]:
         pass
 
     @abstractmethod
-    async def get_unassigned_orders_after(
-        self, last_order_id: int = None, size: int = None
+    async def get_unassigned_orders(
+        self, query: OrderFilterDTO
     ) -> List[OrderWithDetailsAndContractor]:
         pass
 
 
 class ContractorOrderQueryRepository(ABC):
     @abstractmethod
-    async def get_contractor_by_order_id(self, order_id: int) -> Contractor | None:
+    async def get_contractor_by_order_id(self, query: OrderIdDTO) -> Contractor | None:
         pass
 
     @abstractmethod
-    async def filter_contractor_orders(
-        self, contractor_id: int, page: int = 1, size: int = None
-    ) -> List[Order]:
+    async def filter_contractor_orders(self, query: OrderFilterDTO) -> List[Order]:
         pass
 
     @abstractmethod
     async def filter_contractor_detailed_orders(
-        self, contractor_id: int, page: int = 1, size: int = None
+        self, query: OrderFilterDTO
     ) -> List[OrderWithDetailsAndSupervisor]:
         pass
 
 
 class AdminOrderQueryRepository(ABC):
     @abstractmethod
-    async def get_admin_by_order_id(self, order_id: int) -> Admin | None:
+    async def get_admin_by_order_id(self, query: OrderIdDTO) -> Admin | None:
         pass
 
     @abstractmethod
-    async def filter_admin_orders(
-        self, admin_id: int, page: int = 1, size: int = None
-    ) -> List[Order]:
+    async def filter_admin_orders(self, query: OrderFilterDTO) -> List[Order]:
         pass
 
     @abstractmethod
     async def filter_admin_detailed_orders(
-        self, admin_id: int, page: int = 1, size: int = None
+        self, query: OrderFilterDTO
     ) -> List[OrderWithDetailsAndContractor]:
         pass
 
@@ -100,19 +106,19 @@ class AdminOrderQueryRepository(ABC):
 class ContracteeOrderQueryRepository(ABC):
     @abstractmethod
     async def filter_contractee_orders(
-        self, contractee_id: int, page: int = 1, size: int = None
+        self, query: ContracteeOrderFilterDTO
     ) -> List[Order]:
         pass
 
     @abstractmethod
     async def filter_contractee_orders_with_details(
-        self, contractee_id: int, page: int = 1, size: int = None
+        self, query: ContracteeOrderFilterDTO
     ) -> List[OrderWithDetails]:
         pass
 
     @abstractmethod
     async def filter_contractee_complete_orders(
-        self, contractee_id: int, page: int = 1, size: int = None
+        self, query: ContracteeOrderFilterDTO
     ) -> List[CompleteOrder]:
         pass
 
@@ -127,9 +133,9 @@ class OrderCommandRepository(ABC):
         pass
 
     @abstractmethod
-    async def set_order_status(self, order_id: int, status: OrderStatusEnum) -> Order:
+    async def set_order_status(self, query: SetOrderStatusDTO) -> Order:
         pass
 
     @abstractmethod
-    async def set_order_admin(self, order_id: int, admin_id: int) -> Order:
+    async def set_order_admin(self, query: SetOrderAdminDTO) -> Order:
         pass
