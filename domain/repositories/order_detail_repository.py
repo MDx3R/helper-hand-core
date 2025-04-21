f6b2041d6f4c9fa5a4f0d510ea289054384e89e5
MDx3R <nationmdxr@gmail.com>
2025-04-21 12:33:20 +0300

    separate order detail repository interfaces

--- domain/repositories/order_detail_repository.py
+++ domain/repositories/order_detail_repository.py
@@ -1,132 +1,33 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from domain.entities import OrderDetail
+from domain.dto.order.internal.base import DetailIdDTO, OrderIdDTO
+from domain.entities.order.detail import OrderDetail
 
 
-class OrderDetailRepository(ABC):
+class OrderDetailQueryRepository(ABC):
     @abstractmethod
-    async def get_detail_by_id(self, detail_id: int) -> OrderDetail | None:
-        """
-        Получает сведения о заказе по их ID.
-
-        Args:
-            detail_id (int): ID сведений о заказе.
-
-        Returns:
-            OrderDetail: Модель с данными сведений о заказе или `None`, если сведения не найдены.
-
-        Raises:
-            RepositoryException: При всех непредвиденных ошибках.
-        """
+    async def get_detail(self, query: DetailIdDTO) -> OrderDetail | None:
         pass
 
     @abstractmethod
-    async def get_details_by_order_id(
-        self, detail_id: int
-    ) -> OrderDetail | None:
-        """
-        Получает сведения о заказе по их ID.
-
-        Args:
-            detail_id (int): ID сведений о заказе.
-
-        Returns:
-            OrderDetail: Модель с данными сведений о заказе или `None`, если сведения не найдены.
-
-        Raises:
-            RepositoryException: При всех непредвиденных ошибках.
-        """
+    async def get_details_by_order_id(self, query: OrderIdDTO) -> List[OrderDetail]:
         pass
 
     @abstractmethod
-    async def save_detail(self, detail: OrderDetail) -> OrderDetail:
-        """
-        Сохраняет один экземпляр сведения о заказе.
-
-        Args:
-            detail (OrderDetail): сведения о заказе. В объекте должен быть указан `order_id`.
-
-        Returns:
-            OrderDetail: Созданные сведения о заказе.
-
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            RepositoryException: При всех непредвиденных ошибках.
-        """
+    async def get_available_order_details(self, query: OrderIdDTO) -> List[OrderDetail]:
         pass
 
+
+class OrderDetailCommandRepository(ABC):
     @abstractmethod
-    async def save_details(
-        self, details: List[OrderDetail]
-    ) -> List[OrderDetail]:
-        """
-        Сохраняет несколько экземпляров сведений о заказе.
-
-        Args:
-            details (List[OrderDetail]): список сведений о заказе. В объектах должен быть указан `order_id`.
-
-        Returns:
-            List[OrderDetail]: Список созданных сведений о заказе.
-
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            RepositoryException: При всех непредвиденных ошибках.
-        """
+    async def update_detail(self, detail: OrderDetail) -> OrderDetail:
         pass
 
     @abstractmethod
     async def create_detail(self, detail: OrderDetail) -> OrderDetail:
-        """
-        Создает экземпляр сведений о заказе.
-        В отличии от `save_detail` в результате выполнения всегда добавляется новая запись.
-
-        Args:
-            detail (OrderDetail): экземпляр сведений о заказе. В объектах должен быть указан `order_id`.
-
-        Returns:
-            OrderDetail: Созданные сведения о заказе.
-
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            RepositoryException: При всех непредвиденных ошибках.
-        """
         pass
 
     @abstractmethod
-    async def create_details(
-        self, details: List[OrderDetail]
-    ) -> List[OrderDetail]:
-        """
-        Создает несколько экземпляров сведений о заказе.
-        В отличии от `save_details` в результате выполнения всегда добавляются новые записи.
-
-        Args:
-            details (List[OrderDetail]): список сведений о заказе. В объектах должен быть указан `order_id`.
-
-        Returns:
-            List[OrderDetail]: Список созданных сведений о заказе.
-
-        Raises:
-            IntegrityException: Возникает при нарушении целостности данных при сохранении данных.
-            RepositoryException: При всех непредвиденных ошибках.
-        """
-        pass
-
-    @abstractmethod
-    async def get_available_details_by_order_id(
-        self, order_id: int
-    ) -> List[OrderDetail]:
-        """
-        Получает сведения о заказе, имеющие свободные места, по ID заказа.
-
-        Args:
-            order_id (int): ID заказа.
-
-        Returns:
-            List[OrderDetail]: Список доступных сведений о заказе. Результат может быть пустым, если сведения не найдены.
-
-        Raises:
-            RepositoryException: При всех непредвиденных ошибках.
-        """
+    async def create_details(self, details: List[OrderDetail]) -> List[OrderDetail]:
         pass
