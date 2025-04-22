4a95e9a10f7bcae52e61913e4fbff94b36726fe1
MDx3R <nationmdxr@gmail.com>
2025-04-22 13:42:20 +0300

    add method for fetching admin and their contractor profile

--- domain/repositories/user/admin/admin_query_repository.py
+++ domain/repositories/user/admin/admin_query_repository.py
@@ -6,11 +6,18 @@ from domain.dto.user.internal.user_filter_dto import (
     AdminFilterDTO,
 )
 from domain.entities.user.admin import Admin
+from domain.entities.user.composite_admin import AdminWithContractorProfile
 
 
 class AdminQueryRepository(ABC):
     @abstractmethod
     async def get_admin(self, query: UserIdDTO) -> Admin | None:
+        pass
+
+    @abstractmethod
+    async def get_admin_and_contractor_profile(
+        self, query: UserIdDTO
+    ) -> AdminWithContractorProfile | None:
         pass
 
     @abstractmethod
