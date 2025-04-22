6591a82b9d82f7d3bd6bf463a13f5f8c23ebca2b
MDx3R <nationmdxr@gmail.com>
2025-04-22 12:52:32 +0300

    add DetailFilterDTO

--- domain/dto/order/internal/order_filter_dto.py
+++ domain/dto/order/internal/order_filter_dto.py
@@ -7,11 +7,13 @@ class OrderFilterDTO(PaginationDTO):
     status: Optional[OrderStatusEnum] = None
     contractor_id: Optional[int] = None
     admin_id: Optional[int] = None
+    contractee_id: Optional[int] = None
 
     @property
     def supervisor_id(self) -> int:
         return self.admin_id
 
 
-class ContracteeOrderFilterDTO(OrderFilterDTO):
-    contractee_id: int
+class DetailFilterDTO(PaginationDTO):
+    order_id: Optional[int] = None
+    detail_id: Optional[int] = None
