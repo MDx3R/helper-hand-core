6c0d70510d3ccb269eeeb7e7c3269e1b8d65cfde
MDx3R <nationmdxr@gmail.com>
2025-05-11 22:10:28 +0300

    add QueryExecutor

--- infrastructure/repositories/base.py
+++ infrastructure/repositories/base.py
@@ -1,31 +1,52 @@
-from typing import List, TypeVar, Type, Tuple, TypeAlias
-from sqlalchemy import select, exists, and_, Select, Insert, Update, Delete, Sequence
+from typing import Tuple, TypeVar, Generic, Sequence, List, Union
+from sqlalchemy import Select, Insert, Update, Delete, select
 from sqlalchemy.engine import Result, Row
+from sqlalchemy.sql import Executable
 
-from application.transactions import TransactionManager
+from application.transactions.transaction_manager import TransactionManager
 
 O = TypeVar("O", bound=object)
 
-class SQLAlchemyRepository():
+ST = Union[
+    Select[Tuple[O]],
+    Insert[O],
+    Update[O],
+    Delete[O],
+]
+
+
+class QueryExecutor:
     def __init__(self, transaction_manager: TransactionManager):
         self.transaction_manager = transaction_manager
-        
-    def _calculate_offset(self, page: int, size: int) -> int:
-        return (page - 1) * size if size else 0
 
-    async def _execute_scalar_one(self, statement: Select[Tuple[O]] | Insert[Tuple[O]] | Update[Tuple[O]] | Delete[Tuple[O]]) -> O | None:
-        return (await self._execute(statement)).scalar_one_or_none()
-        
-    async def _execute_scalar_many(self, statement: Select[Tuple[O]] | Insert[Tuple[O]] | Update[Tuple[O]] | Delete[Tuple[O]]) -> List[O]:
-        return (await self._execute(statement)).scalars().all()
+    async def execute_scalar_one(
+        self,
+        statement: Select[Tuple[O]],
+    ) -> O | None:
+        return (await self.execute(statement)).scalar_one_or_none()
 
-    async def _execute_one(self, statement: Select[Tuple[O]] | Insert[Tuple[O]] | Update[Tuple[O]] | Delete[Tuple[O]]) -> Row[O]:
-        return (await self._execute(statement)).one_or_none()
+    async def execute_scalar_many(
+        self,
+        statement: Select[Tuple[O]],
+    ) -> List[O]:
+        return (await self.execute(statement)).scalars().all()
 
-    async def _execute_many(self, statement: Select[Tuple[O]] | Insert[Tuple[O]] | Update[Tuple[O]] | Delete[Tuple[O]]) -> Sequence[Row[O]]:
-        return (await self._execute(statement)).all()
-        
-    async def _execute(self, statement: Select[Tuple[O]] | Insert[Tuple[O]] | Update[Tuple[O]] | Delete[Tuple[O]]) -> Result[O]:
+    async def execute_one(
+        self,
+        statement: Select[O],
+    ) -> Row[O] | None:
+        return (await self.execute(statement)).one_or_none()
+
+    async def execute_many(
+        self,
+        statement: Select[O],
+    ) -> Sequence[Row[O]]:
+        return (await self.execute(statement)).all()
+
+    async def execute(
+        self,
+        statement: Select[O] | Insert[O] | Update[O] | Delete[O],
+    ) -> Result[O]:
         async with self.transaction_manager.get_session() as session:
             result = await session.execute(statement)
             return result
