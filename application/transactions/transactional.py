10066fe3bb5edbe3857987646b3ee9ddfb9d71f4
MDx3R <nationmdxr@gmail.com>
2025-05-20 22:11:47 +0300

    add todo to remove self parameter from wrapper

--- application/transactions/transactional.py
+++ application/transactions/transactional.py
@@ -6,6 +6,7 @@ from .configuration import get_transaction_manager
 def transactional(func):
     @functools.wraps(func)
     async def wrapper(self, *args, **kwargs):
+        # TODO: После написания тестов убрать self из wrapper и func для поддерджи функций
         manager = get_transaction_manager()
         if not manager:
             raise ValueError("Не установлен менеджер транзакций.")
